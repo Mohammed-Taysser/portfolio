fa304803a6dfa263f7314cccd6497ff71574f48a
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-07-09 15:15:21 +0300

    create config constant file

---
+++ src/constants/config.ts
@@ -0,0 +1,12 @@
+const CONFIG = {
+	urls: {
+		github: 'https://github.com/mohammed-Taysser',
+		linkedin: 'http://linkedin.com/in/mohammed-taysser',
+		cv: 'https://drive.google.com/file/d/1p6dy_uU1lv6ymVqFgHs03m8aDopRvDHQ/view?usp=sharing',
+	},
+	emails: {
+		gmail: 'mohamedtaysser983@gmail.com',
+	},
+};
+
+export default CONFIG;
