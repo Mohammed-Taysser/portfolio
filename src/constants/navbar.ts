d7c38cc65631332e1c2cd698764c6053e8d538d1
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-07-09 15:44:46 +0300

    create navbar constant

---
+++ src/constants/navbar.ts
@@ -0,0 +1,29 @@
+const NAVIGATION = [
+	{
+		href: 'home',
+		title: 'Home',
+		label: 'Home',
+	},
+	{
+		href: 'about',
+		title: 'About Me',
+		label: 'About',
+	},
+	{
+		href: 'skills',
+		title: 'My Skills',
+		label: 'Skills',
+	},
+	{
+		href: 'projects',
+		title: 'My Projects',
+		label: 'Projects',
+	},
+	{
+		href: 'blogs',
+		title: 'Blogs',
+		label: 'Blogs',
+	},
+];
+
+export { NAVIGATION };
