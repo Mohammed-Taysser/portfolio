c26d35367239e66138aaa2d283c92b988a1336ea
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-07-09 15:13:19 +0300

    create circle-animation component

---
+++ src/components/CircleAnimation.tsx
@@ -0,0 +1,13 @@
+
+function CircleAnimation() {
+	return (
+		<div className='circle-animation'>
+			<div className='circle circle-1'></div>
+			<div className='circle circle-2'></div>
+			<div className='circle circle-3'></div>
+			<div className='circle circle-4'></div>
+		</div>
+	);
+}
+
+export default CircleAnimation;
