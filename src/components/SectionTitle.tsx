b8ebadaa8969b1715456026bc40bf11818b0285e
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-07-09 22:45:54 +0300

    add component docs

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,3 +1,11 @@
+/**
+ * The SectionTitle component is a TypeScript React component that renders a header
+ * with a title and optional subtitle, with the ability to center the text and add
+ * custom CSS classes.
+ * @param {SectionTitleProps} props - The "props" parameter is an object that
+ * contains the properties passed to the SectionTitle component. It is of type
+ * "SectionTitleProps".
+ */
 function SectionTitle(props: SectionTitleProps) {
 	return (
 		<h2
