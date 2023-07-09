b7335f57b31563a1e7506b26110716993bf9d1bf
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-07-09 15:14:00 +0300

    create single-feed component

---
+++ src/components/SingleFeed.tsx
@@ -0,0 +1,26 @@
+function SingleFeed(props: { feed: SingleFeed }) {
+	return (
+		<div className='single-item nice-shadow'>
+			<div className='img'>
+				<img
+					className='img-fluid rounded-top'
+					src={props.feed.img}
+					alt={props.feed.title}
+				/>
+			</div>
+			<div className='content p-4 rounded'>
+				<a
+					className='item-title my-3 fw-bold hovered bg-underline h3'
+					href={props.feed.url}
+					target='_blank'
+					rel='noopener'
+				>
+					{props.feed.title}
+				</a>
+				<p className='text-muted mt-4 mb-0 feed-info'>{props.feed.info}</p>
+			</div>
+		</div>
+	);
+}
+
+export default SingleFeed;
