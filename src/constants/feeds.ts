8389b5fdad2e1c232e235af85e09088fac10c8c3
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-07-09 15:09:15 +0300

    create feeds constant

---
+++ src/constants/feeds.ts
@@ -0,0 +1,16 @@
+import publishNpm from '../assets/images/blogs/how-to-make-your-own-npm-package.webp';
+
+const FEEDS: SingleFeed[] = [
+	{
+		title: 'How To Make Your Own NPM Package',
+		info: `Publishing a NPM package is actually simple, for this tutorial
+		I used a simple example with more complex topics, you can make
+		them as complex as you see fit. Don't forget to add test for
+		the package.`,
+		url: 'https://dev.to/mohammedtaysser/how-to-make-your-own-npm-package-4dm2',
+		img: publishNpm,
+		publishAt: new Date('2023-02-21T18:10:24Z'),
+	},
+];
+
+export { FEEDS };
