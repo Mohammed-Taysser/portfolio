806f4986731884e800d006f78d2a4251b8e3b0e7
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-07-12 20:49:39 +0300

    add noTooltip props

--- src/components/navbar/DarkModeBtn.tsx
+++ src/components/navbar/DarkModeBtn.tsx
@@ -2,14 +2,14 @@ import { UseThemeContext } from '../../context/theme';
 import Moon from '../SVG/navbar/Moon';
 import Sun from '../SVG/navbar/Sun';
 
-function DarkModeBtn() {
+function DarkModeBtn(props: { noTooltip?: boolean }) {
 	const { theme, toggleTheme } = UseThemeContext();
 
 	return (
 		<div
 			className='dark-mode-container'
 			data-bs-title='toggle dark/light mode'
-			data-bs-toggle='tooltip'
+			data-bs-toggle={props.noTooltip ? '' : 'tooltip'}
 			onClick={toggleTheme}
 		>
 			{theme === 'light' ? <Moon /> : <Sun />}
